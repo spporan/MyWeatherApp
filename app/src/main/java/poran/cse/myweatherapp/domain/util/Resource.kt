6a00fcff794b94bafc72d055791b27d416dccf8d
poran <poran.cse@gmail.com>
2022-07-20 21:16:00 +0600

    initial project setup

---
+++ app/src/main/java/poran/cse/myweatherapp/domain/util/Resource.kt
@@ -0,0 +1,8 @@
+package poran.cse.myweatherapp.domain.util
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+
+    class Error<T>(message: String?): Resource<T>(message=message)
+    
+}
